4605fdb88e26feb0c4a29fb55cd8a1cfebf94c42
larryokubasu5460 <bulumalarry3@gmail.com>
2021-04-02 10:58:51 +0300

    names

---
+++ intro/introtwo.py
@@ -0,0 +1,29 @@
+# Print out the output in the following format:
+# =============================================
+
+# Name: Koffi Olomide
+# Country: Democratic Republic of The Congo
+# Profession: Mucisian
+# Email: Koffi.Olomide@koffiolomide.com
+# Phone: +253876564523
+# City: Kinshasa
+# Address: 654E Main Street
+# =============================================
+
+name=input("My names: ")
+country=input('Country of origin: ')
+profession=input('Current profession: ')
+email=input('Email: ')
+phone=input('Phone No: ')
+city=input('City of residence: ')
+address=input('Address: ')
+
+print('='*20)
+print('Name: ',name)
+print('Country: ',country)
+print('Profession: ',profession)
+print('Email: ',email)
+print('Phone: ',phone)
+print('City: ', city)
+print('Address: ',address)
+print('='*20)
